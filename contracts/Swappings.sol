bb53d20e1c264e491b78be86725dc6e3cd5bd2ec
KenatSF <banerabh@gmail.com>
2022-03-14 11:48:07 -0600

    Save extra code

--- contracts/Swappings.sol
+++ contracts/Swappings.sol
@@ -8,18 +8,9 @@ import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 //#########################################################################################################################################
 library Account {
-  enum Status {
-    Normal,
-    Liquid,
-    Vapor
-  }
   struct Info {
     address owner; // The address that owns the account
     uint number; // A nonce that allows a single address to control many accounts
-  }
-  struct accStorage {
-    mapping(uint => Types.Par) balances; // Mapping from marketId to principal
-    Status status;
   }
 }
 //#########################################################################################################################################
@@ -36,18 +27,6 @@ library Actions {
     Call // send arbitrary data to an address
   }
 
-  enum AccountLayout {
-    OnePrimary,
-    TwoPrimary,
-    PrimaryAndSecondary
-  }
-
-  enum MarketLayout {
-    ZeroMarkets,
-    OneMarket,
-    TwoMarkets
-  }
-
   struct ActionArgs {
     ActionType actionType;
     uint accountId;
@@ -59,167 +38,16 @@ library Actions {
     bytes data;
   }
 
-  struct DepositArgs {
-    Types.AssetAmount amount;
-    Account.Info account;
-    uint market;
-    address from;
-  }
-
-  struct WithdrawArgs {
-    Types.AssetAmount amount;
-    Account.Info account;
-    uint market;
-    address to;
-  }
-
-  struct TransferArgs {
-    Types.AssetAmount amount;
-    Account.Info accountOne;
-    Account.Info accountTwo;
-    uint market;
-  }
-
-  struct BuyArgs {
-    Types.AssetAmount amount;
-    Account.Info account;
-    uint makerMarket;
-    uint takerMarket;
-    address exchangeWrapper;
-    bytes orderData;
-  }
-
-  struct SellArgs {
-    Types.AssetAmount amount;
-    Account.Info account;
-    uint takerMarket;
-    uint makerMarket;
-    address exchangeWrapper;
-    bytes orderData;
-  }
-
-  struct TradeArgs {
-    Types.AssetAmount amount;
-    Account.Info takerAccount;
-    Account.Info makerAccount;
-    uint inputMarket;
-    uint outputMarket;
-    address autoTrader;
-    bytes tradeData;
-  }
-
-  struct LiquidateArgs {
-    Types.AssetAmount amount;
-    Account.Info solidAccount;
-    Account.Info liquidAccount;
-    uint owedMarket;
-    uint heldMarket;
-  }
-
-  struct VaporizeArgs {
-    Types.AssetAmount amount;
-    Account.Info solidAccount;
-    Account.Info vaporAccount;
-    uint owedMarket;
-    uint heldMarket;
-  }
-
-  struct CallArgs {
-    Account.Info account;
-    address callee;
-    bytes data;
-  }
+  
 }
 //#########################################################################################################################################
-library Decimal {
-  struct D256 {
-    uint value;
-  }
-}
+//library Decimal {}
 //#########################################################################################################################################
-library Interest {
-  struct Rate {
-    uint value;
-  }
-
-  struct Index {
-    uint96 borrow;
-    uint96 supply;
-    uint32 lastUpdate;
-  }
-}
+//library Interest {}
 //#########################################################################################################################################
-library Monetary {
-  struct Price {
-    uint value;
-  }
-  struct Value {
-    uint value;
-  }
-}
+//library Monetary {}
 //#########################################################################################################################################
-library Storage {
-  // All information necessary for tracking a market
-  struct Market {
-    // Contract address of the associated ERC20 token
-    address token;
-    // Total aggregated supply and borrow amount of the entire market
-    Types.TotalPar totalPar;
-    // Interest index of the market
-    Interest.Index index;
-    // Contract address of the price oracle for this market
-    address priceOracle;
-    // Contract address of the interest setter for this market
-    address interestSetter;
-    // Multiplier on the marginRatio for this market
-    Decimal.D256 marginPremium;
-    // Multiplier on the liquidationSpread for this market
-    Decimal.D256 spreadPremium;
-    // Whether additional borrows are allowed for this market
-    bool isClosing;
-  }
-
-  // The global risk parameters that govern the health and security of the system
-  struct RiskParams {
-    // Required ratio of over-collateralization
-    Decimal.D256 marginRatio;
-    // Percentage penalty incurred by liquidated accounts
-    Decimal.D256 liquidationSpread;
-    // Percentage of the borrower's interest fee that gets passed to the suppliers
-    Decimal.D256 earningsRate;
-    // The minimum absolute borrow value of an account
-    // There must be sufficient incentivize to liquidate undercollateralized accounts
-    Monetary.Value minBorrowedValue;
-  }
-
-  // The maximum RiskParam values that can be set
-  struct RiskLimits {
-    uint64 marginRatioMax;
-    uint64 liquidationSpreadMax;
-    uint64 earningsRateMax;
-    uint64 marginPremiumMax;
-    uint64 spreadPremiumMax;
-    uint128 minBorrowedValueMax;
-  }
-
-  // The entire storage state of Solo
-  struct State {
-    // number of markets
-    uint numMarkets;
-    // marketId => Market
-    mapping(uint => Market) markets;
-    // owner => account number => Account
-    mapping(address => mapping(uint => Account.accStorage)) accounts;
-    // Addresses that can control other users accounts
-    mapping(address => mapping(address => bool)) operators;
-    // Addresses that can control all users accounts
-    mapping(address => bool) globalOperators;
-    // mutable risk parameters of the system
-    RiskParams riskParams;
-    // immutable risk limits of the system
-    RiskLimits riskLimits;
-  }
-}
+//library Storage {}
 //#########################################################################################################################################
 library Types {
   enum AssetDenomination {
@@ -239,194 +67,19 @@ library Types {
     uint value;
   }
 
-  struct TotalPar {
-    uint128 borrow;
-    uint128 supply;
-  }
-
-  struct Par {
-    bool sign; // true if positive
-    uint128 value;
-  }
-
-  struct Wei {
-    bool sign; // true if positive
-    uint value;
-  }
 }
 //#########################################################################################################################################
 interface ISoloMargin {
-  struct OperatorArg {
-    address operator;
-    bool trusted;
-  }
-
-  function ownerSetSpreadPremium(uint marketId, Decimal.D256 calldata spreadPremium)
-    external;
-
-  function getIsGlobalOperator(address operator) external view returns (bool);
-
   function getMarketTokenAddress(uint marketId) external view returns (address);
 
-  function ownerSetInterestSetter(uint marketId, address interestSetter) external;
-
-  function getAccountValues(Account.Info calldata account)
-    external
-    view
-    returns (Monetary.Value memory, Monetary.Value memory);
-
-  function getMarketPriceOracle(uint marketId) external view returns (address);
-
-  function getMarketInterestSetter(uint marketId) external view returns (address);
-
-  function getMarketSpreadPremium(uint marketId)
-    external
-    view
-    returns (Decimal.D256 memory);
-
   function getNumMarkets() external view returns (uint);
-
-  function ownerWithdrawUnsupportedTokens(address token, address recipient)
-    external
-    returns (uint);
-
-  function ownerSetMinBorrowedValue(Monetary.Value calldata minBorrowedValue) external;
-
-  function ownerSetLiquidationSpread(Decimal.D256 calldata spread) external;
-
-  function ownerSetEarningsRate(Decimal.D256 calldata earningsRate) external;
-
-  function getIsLocalOperator(address _owner, address operator)
-    external
-    view
-    returns (bool);
-
-  function getAccountPar(Account.Info calldata account, uint marketId)
-    external
-    view
-    returns (Types.Par memory);
-
-  function ownerSetMarginPremium(uint marketId, Decimal.D256 calldata marginPremium)
-    external;
-
-  function getMarginRatio() external view returns (Decimal.D256 memory);
-
-  function getMarketCurrentIndex(uint marketId)
-    external
-    view
-    returns (Interest.Index memory);
-
-  function getMarketIsClosing(uint marketId) external view returns (bool);
-
-  function getRiskParams() external view returns (Storage.RiskParams memory);
-
-  function getAccountBalances(Account.Info calldata account)
-    external
-    view
-    returns (
-      address[] memory,
-      Types.Par[] memory,
-      Types.Wei[] memory
-    );
-
-  function renounceOwnership() external;
-
-  function getMinBorrowedValue() external view returns (Monetary.Value memory);
-
-  function setOperators(OperatorArg[] calldata args) external;
-
-  function getMarketPrice(uint marketId) external view returns (address);
-
-  function owner() external view returns (address);
-
-  function isOwner() external view returns (bool);
-
-  function ownerWithdrawExcessTokens(uint marketId, address recipient)
-    external
-    returns (uint);
-
-  function ownerAddMarket(
-    address token,
-    address priceOracle,
-    address interestSetter,
-    Decimal.D256 calldata marginPremium,
-    Decimal.D256 calldata spreadPremium
-  ) external;
 
   function operate(
     Account.Info[] calldata accounts,
     Actions.ActionArgs[] calldata actions
   ) external;
 
-  function getMarketWithInfo(uint marketId)
-    external
-    view
-    returns (
-      Storage.Market memory,
-      Interest.Index memory,
-      Monetary.Price memory,
-      Interest.Rate memory
-    );
-
-  function ownerSetMarginRatio(Decimal.D256 calldata ratio) external;
-
-  function getLiquidationSpread() external view returns (Decimal.D256 memory);
-
-  function getAccountWei(Account.Info calldata account, uint marketId)
-    external
-    view
-    returns (Types.Wei memory);
-
-  function getMarketTotalPar(uint marketId)
-    external
-    view
-    returns (Types.TotalPar memory);
-
-  function getLiquidationSpreadForPair(uint heldMarketId, uint owedMarketId)
-    external
-    view
-    returns (Decimal.D256 memory);
-
-  function getNumExcessTokens(uint marketId) external view returns (Types.Wei memory);
-
-  function getMarketCachedIndex(uint marketId)
-    external
-    view
-    returns (Interest.Index memory);
-
-  function getAccountStatus(Account.Info calldata account)
-    external
-    view
-    returns (uint8);
-
-  function getEarningsRate() external view returns (Decimal.D256 memory);
-
-  function ownerSetPriceOracle(uint marketId, address priceOracle) external;
-
-  function getRiskLimits() external view returns (Storage.RiskLimits memory);
-
-  function getMarket(uint marketId) external view returns (Storage.Market memory);
-
-  function ownerSetIsClosing(uint marketId, bool isClosing) external;
-
-  function ownerSetGlobalOperator(address operator, bool approved) external;
-
-  function transferOwnership(address newOwner) external;
-
-  function getAdjustedAccountValues(Account.Info calldata account)
-    external
-    view
-    returns (Monetary.Value memory, Monetary.Value memory);
-
-  function getMarketMarginPremium(uint marketId)
-    external
-    view
-    returns (Decimal.D256 memory);
-
-  function getMarketInterestRate(uint marketId)
-    external
-    view
-    returns (Interest.Rate memory);
+  
 }
 //#########################################################################################################################################
 contract DydxFlashloanBase {
@@ -757,25 +410,12 @@ contract Swappings is ICallee, DydxFlashloanBase {
   }
 
   function callFunction(
-    address sender,
-    Account.Info memory account,
+    address,
+    Account.Info memory,
     bytes memory data
   ) public override {
-    require(msg.sender == SOLO, "!solo");
-    require(sender == address(this), "!this contract");
-
-    MyCustomData memory mcd = abi.decode(data, (MyCustomData));
-    uint repayAmount = mcd.repayAmount;
 
     arb_swap(the_routers[0], the_routers[1], tokens_addresses[0], tokens_addresses[1], quantities[0]);
-   
-    uint bal = IERC20(mcd.token).balanceOf(address(this));
-    require(bal >= repayAmount, "bal < repay");
-
-    // More code here...
-    flashUser = sender;
-    emit Log("bal", bal);
-    emit Log("repay", repayAmount);
-    emit Log("bal - repay", bal - repayAmount);
+    
   }
 }
